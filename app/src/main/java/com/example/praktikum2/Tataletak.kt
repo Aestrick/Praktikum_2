418c664fb677a01dc9c56b9aaa2a70145385bff4
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:36:05 +0700

    Add main Column container for PDF layout

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,9 +1,13 @@
 package com.example.praktikum2
 
+import androidx.compose.foundation.layout.Column // Tambahkan import
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier = Modifier) {
-    // Konten dari PDF akan kita bangun di sini
+    // TAMBAHAN BARU
+    Column(modifier = modifier) {
+        // Box kuning akan masuk di sini
+    }
 }
