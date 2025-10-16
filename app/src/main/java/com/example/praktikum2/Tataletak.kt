3cce8c51fb1ffae389a78df01ad25071bb745f10
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:20:47 +0700

    Add main Column as root layout container

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,8 +1,13 @@
 package com.example.praktikum2
 
+import androidx.compose.foundation.layout.Column // Tambahkan import ini
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun TataLetakModern(modifier: Modifier = Modifier) {
+    // Tambahkan Column di bawah ini
+    Column(modifier = modifier) {
+        // Nanti, semua elemen lain akan kita letakkan di sini
+    }
 }
