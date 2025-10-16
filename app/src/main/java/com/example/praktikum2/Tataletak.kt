3caa202d5d04c56af9d40332cdc1999c23cbc31d
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:39:58 +0700

    Add placeholder image and styled text to cyan Box

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum2
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,12 +9,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier = Modifier) {
@@ -54,7 +60,18 @@ fun TataletakBoxColumnRow(modifier: Modifier = Modifier) {
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
         ) {
-            // Gambar dan teks akan masuk di sini
+            val gambar = Icons.Default.MusicNote // Placeholder untuk notasinaton
+            Image(
+                imageVector = gambar,
+                contentDescription = null
+            )
+            Text(
+                text = "My Music",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive
+            )
         }
     }
 }
