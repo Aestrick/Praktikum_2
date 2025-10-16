28093bfee95353f4aff0d3db8343114ac6294245
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:38:04 +0700

    Add first row of text inside yellow Box

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,9 +1,13 @@
 package com.example.praktikum2
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,14 +17,23 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        // TAMBAHAN BARU
         Box(
             modifier = Modifier
                 .height(110.dp)
+                .fillMaxWidth() // Tambahkan ini agar row bisa menyebar
                 .background(color = Color.Yellow),
             contentAlignment = Alignment.Center
         ) {
-            // Konten di dalam box akan kita tambahkan nanti
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1 Row1 Komponen1")
+                    Text(text = "Col1 Row1 Komponen2")
+                    Text(text = "Col1 Row1 Komponen3")
+                }
+            }
         }
     }
 }
