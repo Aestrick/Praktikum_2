5a58359181f2c06e633a3751d5c5f60987c76472
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:36:32 +0700

    Add first yellow Box container

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,13 +1,26 @@
 package com.example.praktikum2
 
-import androidx.compose.foundation.layout.Column // Tambahkan import
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier = Modifier) {
-    // TAMBAHAN BARU
     Column(modifier = modifier) {
-        // Box kuning akan masuk di sini
+        // TAMBAHAN BARU
+        Box(
+            modifier = Modifier
+                .height(110.dp)
+                .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center
+        ) {
+            // Konten di dalam box akan kita tambahkan nanti
+        }
     }
 }
