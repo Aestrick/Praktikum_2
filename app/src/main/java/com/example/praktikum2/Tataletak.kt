bda7cdeaa4b1a0c0b3c27d9bd23ff776651b3fef
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-16 16:06:07 +0700

    Create Tataletak.kt for UI components

---
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -0,0 +1,9 @@
+package com.example.mylayout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TataLetakModern(modifier: Modifier = Modifier) {
+    // Konten UI akan ditambahkan di sini nanti
+}
